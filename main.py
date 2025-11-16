73bf5b79011e8c6be5334931f57dfca1c379d154
kadirersoy-rgb <kadir.ersoy@edu.esiee.fr>
2025-11-16 14:09:49 +0100

    Kadir ERSOY : Initiation à Dash via un tutoriel simple + Import fichier csv Trafic aérien commercial mensuel français Covid19

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
 #V0 Start of collaboration : ERSOY-POKRYWA ESIEE Paris E3 FI
+# Import packages
+from dash import Dash, html, dcc
+import dash_ag_grid as dag
+import pandas as pd
+import plotly.express as px
+
+df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder2007.csv')
+# Création de l'application Dash
+app = Dash(__name__)
+
+# App layout
+app.layout = [
+    html.Div(children='My First App with Data and a Graph'),
+    dag.AgGrid(
+        rowData=df.to_dict('records'),
+        columnDefs=[{"field": i} for i in df.columns]
+    ),
+    dcc.Graph(figure=px.histogram(df, x='continent', y='lifeExp', histfunc='avg'))
+]
+
+# Lancement de l'application sans debug
+if __name__ == "__main__":
+    app.run() 
