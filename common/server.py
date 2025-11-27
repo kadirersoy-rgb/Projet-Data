2a7b71764ca3c65e6f24254f1a936ca360577e16
kadirersoy-rgb <kadir.ersoy@edu.esiee.fr>
2025-11-27 10:23:17 +0100

    Kadir ERSOY : Premier commit et création serveur Flask

--- common/server.py
+++ common/server.py
@@ -1,3 +1,13 @@
 '''
 Contenu du serveur web
 '''
+from flask import Flask
+
+def creation_serveur():
+    app = Flask(__name__)
+
+    @app.route('/')
+    def welcome():
+        return 'Hello, World!' \
+        
+    app.run(host='0.0.0.0', port=5000, debug=True)
--- main.py
+++ main.py
@@ -3,18 +3,18 @@ from dash import Dash, html, dcc
 import dash_ag_grid as dag
 import pandas as pd
 import plotly.express as px
+from common import server
+# df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder2007.csv')
+# app = Dash(__name__)
 
-df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder2007.csv')
-app = Dash(__name__)
-
-app.layout = [
-    html.Div(children='My First App with Data and a Graph'),
-    dag.AgGrid(
-        rowData=df.to_dict('records'),
-        columnDefs=[{"field": i} for i in df.columns]
-    ),
-    dcc.Graph(figure=px.histogram(df, x='continent', y='lifeExp', histfunc='avg'))
-]
+# app.layout = [
+#     html.Div(children='My First App with Data and a Graph'),
+#     dag.AgGrid(
+#         rowData=df.to_dict('records'),
+#         columnDefs=[{"field": i} for i in df.columns]
+#     ),
+#     dcc.Graph(figure=px.histogram(df, x='continent', y='lifeExp', histfunc='avg'))
+# ]
 
 if __name__ == "__main__":
-    app.run() 
+    server.creation_serveur()
