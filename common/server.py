9c820dba6c7fb294f28cd668ee0bdf0a99549308
kadirersoy-rgb <kadir.ersoy@edu.esiee.fr>
2025-12-02 00:56:08 +0100

    Kadir ERSOY : Creation du serveur Flask + creation d'un header de page d'accueil (index.html)

--- common/server.py
+++ common/server.py
@@ -2,12 +2,18 @@
 Contenu du serveur web
 '''
 from flask import Flask
+from flask import render_template
+import os
 
 def creation_serveur():
-    app = Flask(__name__)
+    projet_folder_base = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    projet_folder_templates = os.path.join(projet_folder_base, "templates")
+    projet_folder_static = os.path.join(projet_folder_base, "static")
+
+    app = Flask(__name__, template_folder=projet_folder_templates, static_folder=projet_folder_static)
 
     @app.route('/')
-    def welcome():
-        return 'Hello, World!' \
+    def index():
+        return render_template('index.html')
         
     app.run(host='0.0.0.0', port=5000, debug=True)
--- main.py
+++ main.py
@@ -1,20 +1,7 @@
-# V0 Start of collaboration : ERSOY-POKRYWA ESIEE Paris E3 FIdir
-from dash import Dash, html, dcc
-import dash_ag_grid as dag
-import pandas as pd
-import plotly.express as px
+# V0 Start of collaboration : ERSOY-POKRYWA-HONODOU ESIEE Paris E3 FI
+#V1 Kadir ERSOY - Lucas POKRYWA - Valentin HONODOU : Creation de l'arborescence du projet
+#V2 Kadir ERSOY : Creation du serveur Flask + creation d'un header de page d'accueil (index.html)
 from common import server
-# df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder2007.csv')
-# app = Dash(__name__)
-
-# app.layout = [
-#     html.Div(children='My First App with Data and a Graph'),
-#     dag.AgGrid(
-#         rowData=df.to_dict('records'),
-#         columnDefs=[{"field": i} for i in df.columns]
-#     ),
-#     dcc.Graph(figure=px.histogram(df, x='continent', y='lifeExp', histfunc='avg'))
-# ]
 
 if __name__ == "__main__":
     server.creation_serveur()
