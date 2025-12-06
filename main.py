eece89fe153f689a243f573a15e2b8a27c2aee9f
kadirersoy-rgb <kadir.ersoy@edu.esiee.fr>
2025-12-07 00:35:53 +0100

    Kadir ERSOY : App Dash + Header mis en place

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
-# V0 Start of collaboration : ERSOY-POKRYWA-HONODOU ESIEE Paris E3 FI
-#V1 Kadir ERSOY - Lucas POKRYWA - Valentin HONODOU : Creation de l'arborescence du projet
-#V2 Kadir ERSOY : Creation du serveur Flask + creation d'un header de page d'accueil (index.html)
 from common import server
+from app import dash1
 from common import data
 
 if __name__ == "__main__":
-    server.creation_serveur()
-    dataframe = data.charger_les_data() #Test chargement data
-    DatFrames_Normalized = data.Normaliser_col_ANMOIS_DateTimes(dataframe) #Test normalisation col ANMOIS en DateTime
+    srv_flask = server.creation_serveur()
+    dash1.creation_app_dash(srv_flask)
 
-    print (DatFrames_Normalized['2018']['ANMOIS'])
+    srv_flask.run(host='0.0.0.0', port=5000, debug=True)
+   
+
+
+   # dataframe = data.charger_les_data() #Test chargement data
+   # DatFrames_Normalized = data.Normaliser_col_ANMOIS_DateTimes(dataframe) #Test normalisation col ANMOIS en DateTime
+
+   # print (DatFrames_Normalized['2018']['ANMOIS'])
