1409605417fb58a87be502788c2a85701dc6ec16
MonkeyLucasTV <lucas.pokrywa@gmail.com>
2025-12-11 11:37:07 +0100

    Modification arborescence dossier data, création d'un fichier de nettoyage telechargement et utilisation de la donnée

---
+++ common/clean_data.py
@@ -0,0 +1,35 @@
+'''
+Prend les fichiers dans les data/raw, formatte les données puis les met dans data/clean
+'''
+
+import os, pandas as pd
+from common import get_data
+
+def Normaliser_ANMOIS():
+    """Normalise la colonne ANMOIS du Dataframe entrée en parametre
+    
+    Arguments:
+    DataFrames -- contient un dataframe de pandas
+    """
+    fichiers = [os.path.join("data/raw", f) for f in os.listdir("data/raw")]
+    for fichier in fichiers:
+        df = pd.read_csv(fichier, sep=";")
+        df['ANMOIS'] = df['ANMOIS'].apply(lambda x: str(x)[:4]+ "-" + str(x)[4:]) 
+        df.to_csv(f'data/clean/{fichier.split("/")[2]}', sep=";", index=False)
+
+def nettoyage_fichier():
+    """ Supprime tout les fichiers dans les dossiers data/raw et data/clean
+    """    
+    dossiers = ["data/raw", "data/clean"]
+    for dossier in dossiers:
+        for fichier in os.listdir(dossier):
+            os.remove(os.path.join(dossier, fichier))
+
+
+def initialiser_donnees():
+    """ Télécharge les données puis les formatte et les mets dans le fichier clean
+    """
+    get_data.charger_donnees()
+    Normaliser_ANMOIS()
+
+
--- common/get_data.py
+++ common/get_data.py
@@ -1,67 +1,34 @@
 '''
 Recuperer les données fetch si elles ou prendre les données dans le répertoires
 '''
-import pandas as pd
-import os
-from typing import List
-import requests 
-import zipfile
-
-def charger_les_data(annees: List[str]) -> None:
-    """Retourne un Dataframe de pandas des fichiers de data
-
-    Arguments:
-    annees -- Liste des années voulues de 2018-2021 ex: ["2018","2021"]
-    """
+import os, requests, zipfile
     
-    projet_folder_data= os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "data")
-    fichiers={}
-    Dataframes={}
-
-    for annee in annees:
-        fichiers[annee] = os.path.join(projet_folder_data, f"{annee}-data.csv")
-
-    for annees, chemin in fichiers.items():
-        df = pd.read_csv(chemin, sep=";")
-        Dataframes[annees] = df
-
-    return Dataframes
-    
-def Normaliser_ANMOIS(DataFrames):
-    """Normalise la colonne ANMOIS du Dataframe entrée en parametre
-    
-    Arguments:
-    DataFrames -- contient un dataframe de pandas
-    """
-
-    conversion = {}
-    for annees, df in DataFrames.items():
-        df = df.copy()
-        df['ANMOIS'] = df['ANMOIS'].apply(lambda x: str(x)[:4]+ "-" + str(x)[4:]) 
-        conversion[annees] = df
-    return conversion
-
-def telecharger_fichier(chemin_fichier, url):
+def telecharger_fichier():
     """ Téléchargement d'un fichier avec un url donné
 
     Arguments:
     chemin_fichier -- Chemin destination du fichier
     url -- url du téléchargement du fichier
     """
+    url = "https://www.data.gouv.fr/api/1/datasets/r/40b43d4b-f7d9-4e7e-9d35-1bfec0aa641d"
     r = requests.get(url)
-    with open(chemin_fichier, "wb") as f:
+    with open("data/raw/data.zip", "wb") as f:
         f.write(r.content)
 
-def unzip_fichier(chemin_fichier, annees):
+def unzip_fichier():
     """  Unzip des fichiers, d'un dossier compressé, avec ses années données
 
     chemin_fichier -- emplacement de l'archive
     annees -- tableau de string d'années voulu
     """
-    with zipfile.ZipFile(chemin_fichier, 'r') as zf:
+    with zipfile.ZipFile("data/raw/data.zip", 'r') as zf:
         for nom in zf.namelist():
-            for annee in annees:
+            for annee in ["2018", "2019", "2020", "2021"]:
                 if annee in nom:
-                    zf.extract(nom, "data")
-                    os.rename(f'data/{nom}', f'data/{annee}-data.csv')
-    os.remove(chemin_fichier)
+                    zf.extract(nom, "data/raw")
+                    os.rename(f'data/raw/{nom}', f'data/raw/{annee}-data.csv')
+    os.remove("data/raw/data.zip")
+
+def charger_donnees():
+    telecharger_fichier()
+    unzip_fichier()
