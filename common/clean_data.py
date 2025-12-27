c6449699eb072f3eb05dbc38d485fd29acb4d64d
MonkeyLucasTV <lucas.pokrywa@gmail.com>
2025-12-27 16:52:37 +0100

    Ajout d'un systeme de region

--- common/clean_data.py
+++ common/clean_data.py
@@ -2,7 +2,7 @@
 Prend les fichiers dans les data/raw, formatte les données puis les met dans data/clean
 '''
 
-import os, pandas as pd
+import json, os, pandas as pd
 from common import get_data
 
 def Normaliser_ANMOIS():
@@ -11,11 +11,26 @@ def Normaliser_ANMOIS():
     Arguments:
     DataFrames -- contient un dataframe de pandas
     """
-    fichiers = [os.path.join("data/raw", f) for f in os.listdir("data/raw")]
+    fichiers = [os.path.join("data/clean", f) for f in os.listdir("data/clean")]
     for fichier in fichiers:
         df = pd.read_csv(fichier, sep=";")
         df['ANMOIS'] = df['ANMOIS'].apply(lambda x: str(x)[:4]+ "-" + str(x)[4:]) 
         df.to_csv(f'data/clean/{fichier.split("/")[2]}', sep=";", index=False)
+
+def AjoutRegion():
+    """ Ajout d'une région pour les aéroport dans le json
+    """
+    with open("region.json", "r", encoding="utf-8") as f:
+        regions = json.load(f)
+
+    fichiers = [os.path.join("data/raw", f) for f in os.listdir("data/raw")]
+    for fichier in fichiers:
+        df = pd.read_csv(fichier, sep=";")
+        df["REGION"] = df["APT_NOM"].map(regions)
+        df["REGION"] = df["REGION"].fillna("Inconnue")
+
+        nom_fichier = os.path.basename(fichier)
+        df.to_csv(f"data/clean/{nom_fichier}", sep=";", index=False)
 
 def nettoyage_fichier():
     """ Supprime tout les fichiers dans les dossiers data/raw et data/clean
@@ -30,3 +45,4 @@ def initialiser_donnees():
     """
     get_data.charger_donnees()
     Normaliser_ANMOIS()
+    AjoutRegion()
